8159975f4fc231abdb00ee81203fbe608c876e81
Enrique Cruz <enrique.cs095@gmail.com>
2023-06-06 09:53:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-const ORIGIN = `https://${process.env.ALIAS_NAME}.${process.env.ENVIRONMENT_NAME}.${process.env.DOMAIN_NAME}`
-
 const port = 1337
 
 app.listen(port, function(error) {
     if (err) console.log(err);
     console.log("Server listening on PORT", port);
-    console.log("Origin", ORIGIN);
 });
 
 app.get('/api', (req, res) => {
-   res.send({'message':'Hello World'});
+  const ORIGIN = `https://${process.env.ALIAS_NAME}.${process.env.ENVIRONMENT_NAME}.${process.env.DOMAIN_NAME}`;
+  res.send({'message': 'Hello World', 'origin': ORIGIN});
 });
+
+
 
