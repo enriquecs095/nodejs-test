0ee8e11546667d2fe833f86debf1f514ad220dc8
Enrique Cruz <enrique.cs095@gmail.com>
2023-06-08 08:33:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-require('dotenv').config();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
